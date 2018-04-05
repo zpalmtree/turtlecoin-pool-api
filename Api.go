b706d3da692d430cb751ba3cbaff66c3f5097c00
ZedPea <zp+github@zacharypalmer.co.uk>
2018-04-05 08:22:28 +0100

    Use v2 pools json

--- Api.go
+++ Api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "strings"
     "net/http"
     "fmt"
     "os"
@@ -11,16 +12,16 @@ import (
     "regexp"
     "strconv"
     "errors"
-    "sort"
     "time"
     "compress/flate"
+    "compress/gzip"
     "bytes"
     "crypto/tls"
     "log"
 )
 
 const poolsJSON string = "https://raw.githubusercontent.com/turtlecoin/" +
-                         "turtlecoin-pools-json/master/turtlecoin-pools.json"
+                         "turtlecoin-pools-json/master/v2/turtlecoin-pools.json"
 
 /* The amount of blocks a pool can vary from the others before we notify */
 const poolMaxDifference int = 5
@@ -30,16 +31,20 @@ const poolRefreshRate time.Duration = time.Second * 30
 
 /* The data type we parse our json into */
 type Pool struct {
-    Api string `json:"url"`
+    Url     string `json:"url"`
+    Api     string `json:"api"`
+    Type    string `json:"type"`
 }
 
 /* Map of pool name to pool api */
-type Pools map[string]Pool
+type Pools struct {
+    Pools   []Pool `json:"pools"`
+}
 
 /* Info about every pool */
 type PoolsInfo struct {
     pools               []PoolInfo
-    medianHeight        int
+    modeHeight          int
     heightLastUpdated   time.Time
 }
 
@@ -98,13 +103,13 @@ func runApi() {
 func forkedHandler(writer http.ResponseWriter, request *http.Request) {
     writer.Header().Set("Content-Type", "application/json")
 
-    median := globalInfo.medianHeight
+    mode := globalInfo.modeHeight
 
     type PoolInfo struct {
         Pool    string
         Reason  string
         Height  int
-        Median  int
+        Mode  int
     }
 
     type DownedInfo struct {
@@ -114,8 +119,8 @@ func forkedHandler(writer http.ResponseWriter, request *http.Request) {
     downedInfo := DownedInfo{Pools: make([]PoolInfo, 0)}
 
     for _, v := range globalInfo.pools {
-        if v.height > median + poolMaxDifference ||
-           v.height < median - poolMaxDifference {
+        if v.height > mode + poolMaxDifference ||
+           v.height < mode - poolMaxDifference {
 
             reason := "forked"
 
@@ -124,7 +129,7 @@ func forkedHandler(writer http.ResponseWriter, request *http.Request) {
             }
 
             pool := PoolInfo{Pool: v.url, Reason: reason, Height: v.height,
-                             Median: median}
+                             Mode: mode}
 
             downedInfo.Pools = append(downedInfo.Pools, pool)
         }
@@ -152,7 +157,7 @@ func lastFoundHandler(writer http.ResponseWriter, request *http.Request) {
 func helpHandler(writer http.ResponseWriter, request *http.Request) {
     fmt.Fprintf(writer, "Supported methods:\n\n" +
                         "/api - Display this help message\n" +
-                        "/api/height - Get median height\n" +
+                        "/api/height - Get mode height\n" +
                         "/api/heights - Get heights of all pools\n" +
                         "/api/lastfound - Display the number of minutes " +
                         "since the last block was found globally\n" +
@@ -162,7 +167,7 @@ func helpHandler(writer http.ResponseWriter, request *http.Request) {
 
 func heightHandler(writer http.ResponseWriter, request *http.Request) {
     writer.Header().Set("Content-Type", "application/json")
-    fmt.Fprintf(writer, "{ \"height\" : %d }", globalInfo.medianHeight)
+    fmt.Fprintf(writer, "{ \"height\" : %d }", globalInfo.modeHeight)
 }
 
 func heightsHandler(writer http.ResponseWriter, request *http.Request) {
@@ -204,9 +209,16 @@ func setup() error {
     poolInfo := make([]PoolInfo, 0)
 
     /* Populate each pool with their info */
-    for url, pool := range pools {
+    for _, pool := range pools.Pools {
         var p PoolInfo
-        p.url = url
+
+        trimmed := pool.Url
+
+        trimmed = strings.TrimPrefix(trimmed, "https://")
+        trimmed = strings.TrimPrefix(trimmed, "http://")
+        trimmed = strings.TrimSuffix(trimmed, "/")
+
+        p.url = trimmed
         p.api = pool.Api
         poolInfo = append(poolInfo, p)
     }
@@ -215,7 +227,7 @@ func setup() error {
     globalInfo.pools = poolInfo
 
     populateHeights()
-    updateMedianHeight()
+    updatemodeHeight()
 
     return nil
 }
@@ -224,7 +236,7 @@ func heightWatcher() {
     for {
         time.Sleep(poolRefreshRate)
         populateHeights()
-        updateMedianHeight()
+        updatemodeHeight()
     }
 }
 
@@ -243,9 +255,16 @@ func poolUpdater() {
         poolInfo := make([]PoolInfo, 0)
 
         /* Populate each pool with their info */
-        for url, pool := range pools {
+        for _, pool := range pools.Pools {
             var p PoolInfo
-            p.url = url
+
+            trimmed := pool.Url
+
+            trimmed = strings.TrimPrefix(trimmed, "https://")
+            trimmed = strings.TrimPrefix(trimmed, "http://")
+            trimmed = strings.TrimSuffix(trimmed, "/")
+
+            p.url = trimmed
             p.api = pool.Api
 
             /* Update it with the local pool info if it exists */
@@ -263,7 +282,7 @@ func poolUpdater() {
         /* Update the global struct */
         globalInfo.pools = poolInfo
         populateHeights()
-        updateMedianHeight()
+        updatemodeHeight()
     }
 }
 
@@ -277,32 +296,40 @@ func getValues(heights map[string]int) []int {
     return values
 }
 
-func updateMedianHeight() {
+func updatemodeHeight() {
     heights := make([]int, 0)
 
     for _, v := range globalInfo.pools {
         heights = append(heights, v.height)
     }
 
-    median := median(heights)
+    mode := mode(heights)
 
-    if median != globalInfo.medianHeight {
-        globalInfo.medianHeight = median
+    if mode != globalInfo.modeHeight {
+        globalInfo.modeHeight = mode
         globalInfo.heightLastUpdated = time.Now()
     }
 }
 
-func median(heights []int) int {
-    sort.Ints(heights)
-
-    half := len(heights) / 2
-    median := heights[half]
-
-    if len(heights) % 2 == 0 {
-        median = (median + heights[half-1]) / 2
+func mode(a []int) int {
+    m := make(map[int]int)
+    for _, v := range a {
+        m[v]++
+    }
+    var mode []int
+    var n int
+    for k, v := range m {
+        switch {
+        case v < n:
+        case v > n:
+            n = v
+            mode = append(mode[:0], k)
+        default:
+            mode = append(mode, k)
+        }
     }
 
-    return median
+    return mode[0]
 }
 
 func populateHeights() {
@@ -321,46 +348,64 @@ func populateHeights() {
     }
 }
 
-func getPoolHeightAndTimestamp (apiURL string) (int, int64, error) {
-    statsURL := apiURL + "stats"
-
-    http.DefaultTransport.(*http.Transport).TLSClientConfig = 
-        &tls.Config{InsecureSkipVerify: true}
-
-    resp, err := http.Get(statsURL)
-
-    if err != nil {
-        fmt.Printf("Failed to download stats from %s! Error: %s\n", 
-                    statsURL, err)
-        return 0, 0, err
-    }
-
-    defer resp.Body.Close()
-
+func getBody (resp *http.Response, statsURL string) ([]byte, error) {
     body, err := ioutil.ReadAll(resp.Body)
 
     if err != nil {
         fmt.Printf("Failed to download stats from %s! Error: %s\n",
                     statsURL, err)
-        return 0, 0, err
+        return nil, err
     }
 
-    /* Some servers (Looking at you us.turtlepool.space! send us deflate'd
+    /* Some servers (Looking at you us.turtlepool.space!) send us deflate'd
        content even when we didn't ask for it - uncompress it */
     if resp.Header.Get("Content-Encoding") == "deflate" {
         body, err = ioutil.ReadAll(flate.NewReader(bytes.NewReader(body)))
 
         if err != nil {
             fmt.Println("Failed to deflate response from", statsURL)
-            return 0, 0, err
+            return nil, err
         }
     }
 
+    /* Some pools like those under the blocks.turtle.link group appear to
+       return multiple values for Content-Encoding, " ", and "gzip" */
+    for k, v := range resp.Header {
+        if k == "Content-Encoding" {
+            for _, v1 := range v {
+                if v1 == "gzip" {
+                    gz, err := gzip.NewReader(bytes.NewReader(body))
+
+                    if err != nil {
+                        fmt.Println("Failed to ungzip response from %s! Error: %s\n",
+                                    statsURL, err)
+                        return nil, err
+                    }
+
+                    defer gz.Close()
+
+                    body, err = ioutil.ReadAll(gz)
+
+                    if err != nil {
+                        fmt.Println("Failed to ungzip response from %s! Error: %s\n",
+                                    statsURL, err)
+                        return nil, err
+                    }
+                    break
+                }
+            }
+        }
+    }
+
+    return body, nil
+}
+
+func parseBody(body string, statsURL string) (int, int64, error) {
     heightRegex := regexp.MustCompile(".*\"height\":(\\d+).*")
     blockFoundRegex := regexp.MustCompile(".*\"lastBlockFound\":\"(\\d+)\".*")
 
-    height := heightRegex.FindStringSubmatch(string(body))
-    blockFound := blockFoundRegex.FindStringSubmatch(string(body))
+    height := heightRegex.FindStringSubmatch(body)
+    blockFound := blockFoundRegex.FindStringSubmatch(body)
 
     if len(height) < 2 {
         fmt.Println("Failed to parse height from", statsURL)
@@ -391,6 +436,43 @@ func getPoolHeightAndTimestamp (apiURL string) (int, int64, error) {
     }
 
     return i, unix, nil
+}
+
+func getPoolHeightAndTimestamp (apiURL string) (int, int64, error) {
+    statsURL := apiURL + "stats"
+
+    http.DefaultTransport.(*http.Transport).TLSClientConfig = 
+        &tls.Config{InsecureSkipVerify: true}
+
+    timeout := time.Duration(5 * time.Second)
+
+    client := http.Client {
+        Timeout: timeout,
+    }
+
+    resp, err := client.Get(statsURL)
+
+    if err != nil {
+        fmt.Printf("Failed to download stats from %s! Error: %s\n", 
+                    statsURL, err)
+        return 0, 0, err
+    }
+
+    defer resp.Body.Close()
+
+    body, err := getBody(resp, statsURL)
+
+    if err != nil {
+        return 0, 0, err
+    }
+
+    height, unix, err := parseBody(string(body), statsURL)
+
+    if err != nil {
+        return 0, 0, err
+    }
+
+    return height, unix, nil
 }
 
 func getPools() (Pools, error) {
